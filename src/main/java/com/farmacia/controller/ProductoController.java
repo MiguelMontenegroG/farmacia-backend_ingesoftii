51867b7bb60e35ba02b170eef0ae9dfacd8f0279
Luis Carlos Morales C. <luisc.moralesc@uqvirtual.edu.co>
2025-09-27 19:13:31 -0500

    Api crear producto

---
+++ src/main/java/com/farmacia/controller/ProductoController.java
@@ -0,0 +1,28 @@
+package com.farmacia.controller;
+
+import com.farmacia.model.Producto;
+import com.farmacia.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    // Obtener todos los productos
+    @GetMapping
+    public List<Producto> getProductos() {
+        return productoService.obtenerCatalogo();
+    }
+
+    // Crear producto
+    @PostMapping
+    public Producto crearProducto(@RequestBody Producto producto) {
+        return productoService.guardarProducto(producto);
+    }
+}
