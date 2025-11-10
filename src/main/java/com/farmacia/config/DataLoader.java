b2254f87014614c51515745da833ad47bef8ab05
MiguelMontenegroG <miguela.montenegrog@uqvirtual.edu.co>
2025-11-10 08:49:25 -0500

    dataloader

--- src/main/java/com/farmacia/config/DataLoader.java
+++ src/main/java/com/farmacia/config/DataLoader.java
@@ -1,8 +1,17 @@
-packagecom.farmacia.config;
+package com.farmacia.config;
 
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataLoader {
-
+    // DataLoader desactivado temporalmente para resolver problemas de inicialización
+    // La implementación original se encuentra comentada abajo
+    
+    /*
+    private final CategoriaRepository categoriaRepository;
+    private final ProductoRepository productoRepository;
+    
+    // Constructor y métodos de inicialización de datos
+    // ...
+    */
 }
