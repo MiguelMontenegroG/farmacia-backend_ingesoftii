af6f2248a914c522b88a3a92cd9171ecf0535d0b
MiguelMontenegroG <miguela.montenegrog@uqvirtual.edu.co>
2025-11-10 10:50:14 -0500

    Version estable baack en local

--- src/main/java/com/farmacia/security/WebSecurityConfig.java
+++ src/main/java/com/farmacia/security/WebSecurityConfig.java
@@ -10,11 +10,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class SecurityConfig {
+public class WebSecurityConfig {
 
     @Value("${app.cors.allowed-origins:http://localhost:3000}")
     private List<String> allowedOrigins;
